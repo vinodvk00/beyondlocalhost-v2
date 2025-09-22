041fc6c5f313084e4b291b3651459a6bbb85811b
vinod <21r21a3344@mlrinstitutions.ac.in>
2025-09-22 19:45:55 +0530

    fix: scrolling in main layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -38,9 +38,7 @@ export default function RootLayout({
                     <AuthProvider>
                         <div className="flex h-screen flex-col">
                             <Header />
-                            <main className="flex-1 overflow-hidden">
-                                {children}
-                            </main>
+                            <main className="flex-1">{children}</main>
                         </div>
                         <Toaster />
                     </AuthProvider>
